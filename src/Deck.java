c898de92b6201e11cf18c1bbb94a67b03008299e
SN41 <andrey.sn41@gmail.com>
2025-12-15 18:56:05 +0500

    Добавлены очки карт, полностью

--- src/Deck.java
+++ src/Deck.java
@@ -4,12 +4,12 @@ public class Deck {
     boolean[] exists = new boolean[36];
 
     {
-        for (int i = 0; i<36;i++){
+        for (int i = 0; i < 36; i++) {
             exists[i] = true;
         }
     }
 
-    int get(){
+    int get() {
         Random r = new Random();
         while (true) {
             int id = r.nextInt(0, 36);
@@ -21,7 +21,7 @@ public class Deck {
         }
     }
 
-    Card getCard(){
+    Card getCard() {
         int id = get();
         Card card = cards[id];
         return card;
@@ -37,33 +37,33 @@ public class Deck {
             new Card("♠", " В", 2),
             new Card("♠", " Д", 3),
             new Card("♠", " К", 4),
-            new Card("♣", " 6"),
-            new Card("♣", " 7"),
-            new Card("♣", " 8"),
-            new Card("♣", " 9"),
-            new Card("♣", "10"),
-            new Card("♣", " В"),
-            new Card("♣", " Д"),
-            new Card("♣", " К"),
-            new Card("♣", " Т"),
-            new Card("♦", " 6"),
-            new Card("♦", " 7"),
-            new Card("♦", " 8"),
-            new Card("♦", " 9"),
-            new Card("♦", "10"),
-            new Card("♦", " В"),
-            new Card("♦", " Д"),
-            new Card("♦", " К"),
-            new Card("♦", " Т"),
-            new Card("♥", " 6"),
-            new Card("♥", " 7"),
-            new Card("♥", " 8"),
-            new Card("♥", " 9"),
-            new Card("♥", "10"),
-            new Card("♥", " В"),
-            new Card("♥", " Д"),
-            new Card("♥", " К"),
-            new Card("♥", " Т"),
+            new Card("♣", " 6", 6),
+            new Card("♣", " 7", 7),
+            new Card("♣", " 8", 8),
+            new Card("♣", " 9", 9),
+            new Card("♣", "10", 10),
+            new Card("♣", " Т", 1),
+            new Card("♣", " В", 2),
+            new Card("♣", " Д", 3),
+            new Card("♣", " К", 4),
+            new Card("♦", " 6", 6),
+            new Card("♦", " 7", 7),
+            new Card("♦", " 8", 8),
+            new Card("♦", " 9", 9),
+            new Card("♦", "10", 10),
+            new Card("♦", " Т", 1),
+            new Card("♦", " В", 2),
+            new Card("♦", " Д", 3),
+            new Card("♦", " К", 4),
+            new Card("♥", " 6", 6),
+            new Card("♥", " 7", 7),
+            new Card("♥", " 8", 8),
+            new Card("♥", " 9", 9),
+            new Card("♥", "10", 10),
+            new Card("♥", " В", 2),
+            new Card("♥", " Д", 3),
+            new Card("♥", " К", 4),
+            new Card("♥", " Т", 1),
     };
 
 
